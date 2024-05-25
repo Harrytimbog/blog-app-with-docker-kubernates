fb57afcafe18064c8c95b07d76e4827c746ce5ba
Timilehin Arigbede <harriemannie@gmail.com>
2024-05-25 18:21:22 +0100

    Parsing Incoming Events

--- query/index.js
+++ query/index.js
@@ -6,10 +6,44 @@ const app = express();
 app.use(bodyParser.json());
 app.use(cors());
 
-app.get("/posts", (req, res) => {});
+const posts = {};
 
-app.get("/events", (req, res) => {});
+app.get("/posts", (req, res) => {
+  res.send(posts);
+});
+
+app.post("/events", (req, res) => {
+  const { type, data } = req.body;
+
+  if (type === "PostCreated") {
+    const { id, title } = data;
+    posts[id] = { id, title, comments: [] };
+  }
+  if (type === "CommentCreated") {
+    const { id, content, postId } = data;
+
+    const post = posts[postId];
+    post.comments.push({ id, content });
+  }
+  console.log(posts);
+  res.send({});
+});
 
 app.listen(4002, () => {
   console.log("Listening on 4002");
 });
+
+// data example
+// posts ===
+//   {
+//     j123j22: {
+//       id: "j123j22",
+//       title: "post title",
+//       comments: [{ id: "kididi", content: "comment!" }],
+//     },
+//     js3344l: {
+//       id: "js3344l",
+//       title: "post title",
+//       comments: [{ id: "kiyey55", content: "comment!" }],
+//     },
+//   };
